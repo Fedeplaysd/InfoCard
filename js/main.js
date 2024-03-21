2fd8967b4496ba288c46ae242064d222599adcc9
Fedeplaysd <fedpedrazaimb@gmail.com>
2024-03-21 16:59:16 -0300

    Etapa Produccion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 function enviarMensajeWhatsApp() {
   window.location.href = "https://wa.me/+543571515710?text=Gracias por comunicarte! En breve atendere tu mensaje.";
 }
+function ingresarPaginaLinkedIn() {
+  window.location.href = "https://www.linkedin.com/in/federico-pedraza-arg/"; 
+}
+function ingresarPaginaFiver() {
+  window.location.href = "https://www.fiverr.com/pedrazawebdev/create-your-basic-website-with-coustom-desings?utm_medium=shared&utm_source=copy_link&utm_campaign=base_gig_create_share&utm_term=0Z9zWL&view=gig&gig_id=365709919"; 
+}
